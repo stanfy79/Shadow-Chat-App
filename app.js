6762ae2257bb732feb4a18053f19b2eb11471482
Standfirm <standfirmbiokoro79@gmail.com>
2024-05-26 00:47:08 +0100

    Udated backend

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, push, onChildAdded } from 'firebase/database';
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Firebase configuration
 const appSettings = {
